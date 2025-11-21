2bde23825e92c96d9a10e7c2067b70abb7eb9646
Oliveresm <oliveresm@outlook.com>
2025-11-20 21:27:56 -0600

    fix: Add missing portal ECR repository definition

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,9 +1,6 @@
-# --- 1. Repositorio ECR para el "Processor" ---
-# ECR (Elastic Container Registry) es el Docker Hub privado de AWS.
+# --- 1. Repositorio ECR para el "Processor" (Worker) ---
 resource "aws_ecr_repository" "processor_repo" {
   name = "healthtrends/processor"
-
-  # Fuerza la eliminación aunque haya imágenes (para desarrollo)
   force_delete = true
 
   tags = {
@@ -11,8 +8,22 @@ resource "aws_ecr_repository" "processor_repo" {
   }
 }
 
-# --- 2. Outputs ---
 output "ecr_processor_repo_url" {
   description = "La URL del repositorio ECR del Processor"
   value       = aws_ecr_repository.processor_repo.repository_url
+}
+
+# --- 2. Repositorio ECR para el "Portal" (API) ---
+# ESTA ES LA PARTE QUE TE FALTABA
+resource "aws_ecr_repository" "portal_repo" {
+  name = "healthtrends/portal"
+  force_delete = true
+
+  tags = {
+    Name = "healthtrends-portal-repo"
+  }
+}
+
+output "ecr_portal_repo_url" {
+  value = aws_ecr_repository.portal_repo.repository_url
 }
