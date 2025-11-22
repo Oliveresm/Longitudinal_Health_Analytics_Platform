6b552009155d25951b62d03b1131fbf238ef04ca
samanthaMora <samantha.mora@edu.uag.mx>
2025-11-22 13:09:08 -0600

    feat: Add Admins group and IAM permissions for user management

--- healthtrends-frontend/src/main.jsx
+++ healthtrends-frontend/src/main.jsx
@@ -1,8 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' 
+import './index.css'
+import { Amplify } from 'aws-amplify';
 
-createRoot(document.getElementById('root')).render(
+// --- DIAGNÓSTICO: VERIFICAR VARIABLES ---
+console.log("User Pool ID:", import.meta.env.VITE_COGNITO_USER_POOL_ID);
+console.log("Client ID:", import.meta.env.VITE_COGNITO_APP_CLIENT_ID);
+// ----------------------------------------
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
+      userPoolClientId: import.meta.env.VITE_COGNITO_APP_CLIENT_ID,
+    }
+  }
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
 )
