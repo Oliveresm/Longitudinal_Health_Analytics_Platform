34f669fd861e5ffca2d87175f0fb7c679525e735
Oliver Suarez Mora <oliveresm@outlook.com>
2025-12-01 01:11:55 -0600

    update proyect

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,3 +4,12 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1" # O la región que prefieras
 }
+
+# Archivo: variables.tf
+
+variable "environment" {
+  description = "El ambiente de despliegue (ej: dev, staging, prod)."
+  type        = string
+  # Puedes establecer un valor por defecto si no quieres pasarlo por línea de comandos
+  default     = "dev" 
+}
